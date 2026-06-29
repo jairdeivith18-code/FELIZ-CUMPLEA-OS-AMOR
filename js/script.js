7814333a3363a73fe39c15653501dfa88c2c8232
Jair Deivith <jairdeivith18@gmail.com>
2026-06-29 17:53:48 -0500

    Versión 2.1 - Mejoras en la historia

--- js/script.js
+++ js/script.js
@@ -1,28 +1,62 @@
-window.addEventListener("load",()=>{
+const boton = document.getElementById("btnComenzar");
 
-const overlay=document.querySelector(".overlay");
-
-overlay.style.opacity="0";
-overlay.style.transform="translateY(50px)";
-
-setTimeout(()=>{
-
-overlay.style.transition="1.5s";
-
-overlay.style.opacity="1";
-
-overlay.style.transform="translateY(0px)";
-
-},300);
-
-});
-
-document.getElementById("btnComenzar").addEventListener("click",()=>{
+boton.addEventListener("click", ()=>{
 
 document.getElementById("historia").scrollIntoView({
 
 behavior:"smooth"
 
 });
+
+});
+
+
+// CORAZONES
+
+const contenedor=document.querySelector(".corazones");
+
+function crearCorazon(){
+
+const corazon=document.createElement("div");
+
+corazon.classList.add("corazon");
+
+corazon.innerHTML="❤";
+
+corazon.style.left=Math.random()*100+"vw";
+
+corazon.style.animationDuration=(4+Math.random()*4)+"s";
+
+corazon.style.fontSize=(15+Math.random()*20)+"px";
+
+contenedor.appendChild(corazon);
+
+setTimeout(()=>{
+
+corazon.remove();
+
+},8000);
+
+}
+
+setInterval(crearCorazon,350);
+
+// ===============================
+// ANIMACIÓN AL HACER SCROLL
+// ===============================
+
+const historia = document.querySelector(".contenido");
+
+window.addEventListener("scroll",()=>{
+
+const posicion = historia.getBoundingClientRect().top;
+
+const pantalla = window.innerHeight;
+
+if(posicion < pantalla - 150){
+
+historia.classList.add("mostrar");
+
+}
 
 });
